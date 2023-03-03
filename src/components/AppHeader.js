57d2465114e19de9b17a6869ab5d33768841f8d2
hasibulislam999 <hasib143sl@gmail.com>
2023-03-03 16:35:15 +0600

    app header components modified by quotes

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Button, { SelectButton } from "./Button";
-import styles from "../styles/modules/app.module.scss";
-import TodoModal from "./TodoModal";
-import { updateFilterStatus } from "../slices/todoSlice";
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Button, { SelectButton } from './Button';
+import styles from '../styles/modules/app.module.scss';
+import TodoModal from './TodoModal';
+import { updateFilterStatus } from '../slices/todoSlice';
 
 function AppHeader() {
   const [modalOpen, setModalOpen] = useState(false);
